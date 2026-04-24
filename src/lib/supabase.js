c198578844d5d8856b336b355be8a7b514dae442
ceimariasalete <ceimariasalete1@gmail.com>
2026-04-24 13:53:22 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.MODE === 'development'
-  ? "http://localhost:5173/supabase"
-  : import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  supabaseAnonKey
 );
